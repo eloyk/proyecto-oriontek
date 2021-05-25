53a20450a3ea5d7eac93a192a7fa81a380c37ef4
eloyk <stifler9.0@hotmail.com>
2021-05-25 01:55:50 -0400

    Organizacion de carpetas

--- back-end/index.ts
+++ back-end/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import config from './config/config';
 
 const iniciar = async () => {
-  console.log('Iniciando Bella Bella Boutique');
+  console.log('Iniciando Empresa X');
 
 try{  
   await mongoose.connect(config.DB.URI, {
